965bed2c8bba31f1ccd5f9c3c2fbb9506c837715
Ian Angillis <ianangillis@gmail.com>
2023-04-30 16:06:36 +0200

    more work

---
+++ tool/convert_sf_dataset_to_dockerfiles.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const content = fs.readFileSync("./data/dataset_dockerfile.pb");
+
+function extractCodeTags(str) {
+    const dockerfileRegex = /<code>(FROM|MAINTAINER|RUN|CMD|EXPOSE|ENV|ADD|COPY|ENTRYPOINT|VOLUME|USER|WORKDIR|ARG|ONBUILD|#)(.*?)<\/code>/gs;
+    const codeMatches = str.match(dockerfileRegex);
+    return codeMatches.map((match) => match.replace(/<\/?code>/g, ''));
+}
+
+let keywords = ["FROM", "MAINTAINER", "RUN", "CMD", "EXPOSE", "ENV", "ADD", "COPY", "ENTRYPOINT", "VOLUME", "USER", "WORKDIR", "ARG", "ONBUILD", "#"];
+let extracted = extractCodeTags(content.toString());
+let num = 0;
+extracted.forEach(code => {
+    let shouldBeFile = true;
+    code = code.replace(/&amp;/g, "&");
+
+    keywords.forEach(keyword => {
+        if(keyword == code){
+            shouldBeFile = false;
+        }
+    });
+
+    if(shouldBeFile){
+        fs.writeFileSync("./../data/stackoverflow/sf_docker_snippet_" + num.toString() + ".Dockerfile", code.toString());
+        num = num + 1;
+    }
+});
